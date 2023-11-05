19d9c194a05426f65fc5512c35064a7b521d4d2c
HaroldC8 <pogojus.haroldas@gmail.com>
2023-11-05 22:25:44 +0200

    Added icon and started work on problem button

--- popup.js
+++ popup.js
@@ -1,7 +1,45 @@
+import * as hfs from "./hash-functions.js";
+
+const tags = [
+    '2-sat',
+    'binary search',
+    'bitmasks',
+    'brute force',
+    'combinatorics',
+    'constructive algorithms',
+    'data structures',
+    'dfs and similar',
+    'divide and conquer',
+    'dp',
+    'dsu',
+    'expression parsing',
+    'fft',
+    'flow',
+    'games',
+    'geometry',
+    'graph matchings',
+    'graphs',
+    'greedy',
+    'hashing',
+    'implementation',
+    'interactive',
+    'math',
+    'matrices',
+    'number theory',
+    'probabilities',
+    'schedules',
+    'shortest paths',
+    'sortings',
+    'strings',
+    'ternary search',
+    'trees',
+    'two pointers'
+];
+
 const generate = document.getElementById('generateButton');
 const url = document.getElementById('problemUrl');
 const rating = document.getElementById('problemRating');
-const tags = document.getElementById('problemTags');
+//const problemTags = document.getElementById('problemTags');
 
 const apiUrl = "https://codeforces.com/api/problemset.problems";
 const problemsUrl = "https://codeforces.com/problemset/problem/"
@@ -10,71 +48,40 @@ generate.addEventListener('click', () => {
     fetchUrl(false);
 });
 
-window.onbeforeunload = fetchUrl;
 window.onload = fetchUrl;
 
-function fetchUrl(fromDate = true) {
+function fetchUrl(daily = true) {
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         var problems = data.result.problems;
         var problem = problems[Math.floor(Math.random()*problems.length)];
 
-        if(fromDate) {
-            let dateObj = new Date();
-            let month = dateObj.getUTCMonth() + 1;
-            let day = dateObj.getUTCDate();
-            let year = dateObj.getUTCFullYear();
+        if(daily) {
 
-            var seed = cyrb128(year + "/" + month + "/" + day);
-            var rand = sfc32(seed[0], seed[1], seed[2], seed[3]);
+            var seed = hfs.cyrb128(dateToString());
+            var rand = hfs.sfc32(seed[0], seed[1], seed[2], seed[3]);
             
             problem = problems[Math.floor(rand()*problems.length)];
         }
-        console.log(problem);
         url.innerHTML = problem.index + ". " + problem.name;
         url.href = problemsUrl + problem.contestId + "/" + problem.index;
         rating.innerHTML = "Rating: " + problem.rating;
-        let alltags = "";
+        /*let alltags = "";
         for(let i = 0; i < problem.tags.length; i++) {
             alltags += problem.tags[i] + ", ";
         }
-        tags.innerHTML = "Tags: " + alltags.slice(0, alltags.length-2);
+        problemTags.innerHTML = "Tags: " + alltags.slice(0, alltags.length-2);*/
     })
     .catch(error => {
         console.log("Something went wrong:", error);
     })
 }
 
-//https://stackoverflow.com/questions/521295/seeding-the-random-number-generator-in-javascript
-function cyrb128(str) {
-    let h1 = 1779033703, h2 = 3144134277,
-        h3 = 1013904242, h4 = 2773480762;
-    for (let i = 0, k; i < str.length; i++) {
-        k = str.charCodeAt(i);
-        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
-        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
-        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
-        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
-    }
-    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
-    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
-    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
-    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
-    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
-    return [h1>>>0, h2>>>0, h3>>>0, h4>>>0];
-}
-
-function sfc32(a, b, c, d) {
-    return function() {
-      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
-      var t = (a + b) | 0;
-      a = b ^ b >>> 9;
-      b = c + (c << 3) | 0;
-      c = (c << 21 | c >>> 11);
-      d = d + 1 | 0;
-      t = t + d | 0;
-      c = c + t | 0;
-      return (t >>> 0) / 4294967296;
-    }
+function dateToString() {
+    let dateObj = new Date();
+    let month = dateObj.getUTCMonth() + 1;
+    let day = dateObj.getUTCDate();
+    let year = dateObj.getUTCFullYear();
+    return (year + "/" + month + "/" + day);
 }
